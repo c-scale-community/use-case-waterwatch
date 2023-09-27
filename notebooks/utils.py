a9b7b9c9c8ddae45cdcb743d0a818079d6aeca3b
Jaap Langemeijer <jaaplangemeijer@gmail.com>
2023-09-27 15:27:16 +0200

    replace client with anonymous client

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -19,34 +19,34 @@ class Reservoir:
         source_id: Optional[str] = None,
         name: Optional[str] = None,
         name_en: Optional[str] = None,
-        grand_id: int = None
+        grand_id: int = None,
     ) -> "Reservoir":
         self._fid = fid
         self._source_name = source_name
         self._source_id = source_id
         self._geometry = geometry
-        self._name = name,
-        self._name_en = name_en,
+        self._name = (name,)
+        self._name_en = (name_en,)
         self._grand_id = grand_id
 
     @property
     def geometry(self) -> Union[MultiPolygon, Polygon]:
         return self._geometry
-    
+
     @geometry.setter
     def geometry(self, geometry: Union[MultiPolygon, Polygon]):
         self._geometry = geometry
-    
+
     @property
     def fid(self) -> int:
         return self._fid
-    
+
     @classmethod
     def from_gcp(
         cls: "Reservoir",
         out_dir: Path,
         gcp_project: str = "global-water-watch",
-        gcp_bucket: str = "global-water-watch"
+        gcp_bucket: str = "global-water-watch",
     ) -> List["Reservoir"]:
         """
         downloads shape files from gcp bucket source and creates a list of available Reservoirs.
@@ -56,15 +56,17 @@ class Reservoir:
             gcp_project (str): Google Cloud Platform Project.
             gcp_bucket (str): Google Cloud Platform Bucket.
         """
-        os.environ["GCLOUD_PROJECT"] = gcp_project
 
-        client: Client = Client()
+        client: Client = Client.create_anonymous_client()
+
         bucket: Bucket = Bucket(client, name=gcp_bucket, user_project=gcp_project)
 
         blobs = client.list_blobs(bucket, prefix="shp/reservoirs-v1.0")
         for b in blobs:
-            b.download_to_filename(out_dir / b.name.split("/")[-1], )
-            
+            b.download_to_filename(
+                out_dir / b.name.split("/")[-1],
+            )
+
         shapes = fiona.open(out_dir / "reservoirs-v1.0.shp")
 
         def process_shapes(shape: dict):
@@ -74,7 +76,7 @@ class Reservoir:
                 geometry: MultiPolygon = MultiPolygon([Polygon(c[0]) for c in coords])
             elif s_type == "Polygon":
                 geometry: Polygon = Polygon(coords[0])
-            
+
             properties: dict = shape["properties"]
             return cls(
                 fid=properties["fid"],
@@ -83,7 +85,7 @@ class Reservoir:
                 source_id=properties["source_id"],
                 name=properties["name"],
                 name_en=properties["name_en"],
-                grand_id=properties["grand_id"]
+                grand_id=properties["grand_id"],
             )
 
         return list(map(process_shapes, iter(shapes)))
